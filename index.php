92f37ad15db780497b78951c5bba7e5cda461d20
Himani <64135241+HimaniBhaisare@users.noreply.github.com>
2020-08-02 09:45:00 +0530

    added matter to front page
    changed text.

--- index.php
+++ index.php
@@ -5,7 +5,15 @@ $content=<<<_END
 <div class="row">
     <div class="col-sm-6">
         <h3>Customers</h3>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae tempora accusantium eaque dolore ipsa et voluptatum dicta asperiores illo deserunt quod incidunt ducimus fugit quasi veniam culpa, nam porro impedit!
+        Welcome! 
+        We are here to provide you water in the hour of need. The main idea of this system is to be able to utilise rainwater to its maximum and make it available to the people directly through our portal. 
+        We take pride in the fact that this scheme could be a life saver if implemented properly in the cities its needed the most. 
+        All we do is this,
+        -Join hands with the institutions willing to contribute and provide them subsidy to setup the rainwater harvesting unit.
+        - Make use of this harvested water to provide it to the people on demand.
+
+        Make sure to join hands with us! That is, if you want your city to sustain.
+
     </div>
     <div class="col-sm-6">
         <a href="customers/dashboard.php" class="btn btn-success" style="text-align: center;">Book Tanker</a>
